be1214f249d09faaa95b000b8b2d4c14bd3d1a08
khaifung <khaifunglim97@gmail.com>
2024-08-30 18:26:17 +0800

    [swipeable_page_route] fix: provider gesture variables are not updated correctly

--- swipeable_page_route/lib/src/page_route.dart
+++ swipeable_page_route/lib/src/page_route.dart
@@ -417,8 +417,8 @@ class SwipeableBackGestureDetector extends ChangeNotifier {
   }
 
   void updateGesturesPositionFromLRTB({Rect? gesture1Rect, Rect? gesture2Rect}) {
-    gesture1Rect = gesture1Rect;
-    gesture2Rect = gesture2Rect;
+    this.gesture1Rect = gesture1Rect;
+    this.gesture2Rect = gesture2Rect;
     notifyListeners();
   }
 }
@@ -607,6 +607,7 @@ class _BackDetector extends StatelessWidget {
       builder: (_, detector, child) {
         final gesture1 = detector.gesture1Rect ?? gesture1Rect;
         final gesture2 = detector.gesture2Rect ?? gesture2Rect;
+        print('gesture1: $gesture1, gesture2: $gesture2');
         return detector.isDetecting && child != null
             ? Stack(
                 fit: StackFit.passthrough,
